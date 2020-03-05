130a9fcb709c5e3e46a28e8c67103562d8c7742c
gull <golgizumi@gmail.com>
2020-03-05 21:10:24 +0500

    first commit

---
+++ subpages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home' ),
+    path('about/', views.AboutMePage.as_view(), name='about')
+]
